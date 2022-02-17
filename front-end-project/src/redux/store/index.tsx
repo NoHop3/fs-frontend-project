d0d01395159a57705f4831bdad755d31aa547353
NoHop3 <u_sux@abv.bg>
2022-02-17 19:15:32 +0100

    Just place local storage from lecture dont want to bother more with it

--- front-end-project/src/redux/store/index.tsx
+++ front-end-project/src/redux/store/index.tsx
@@ -1,8 +1,18 @@
 import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+import { RootState } from "../../typescript/redux/store";
 
 import rootReducer from "../reducers";
+
+function saveToLocalStorage(state: RootState) {
+  try {
+    const localStorageState = JSON.stringify(state);
+    localStorage.setItem("AppState", localStorageState);
+  } catch (exception) {
+    console.log(exception);
+  }
+}
 
 const store = () => {
   const middleware = [thunk];
@@ -10,6 +20,7 @@ const store = () => {
     rootReducer,
     composeWithDevTools(applyMiddleware(...middleware))
   );
+  reduxStore.subscribe(() => saveToLocalStorage(reduxStore.getState()));
   return reduxStore;
 };
 
